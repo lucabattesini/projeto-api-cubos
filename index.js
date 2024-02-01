7e575e63accfe2c76740ac3be613b100dd66a773
Luca Battesini <lucabattesini1@gmail.com>
2024-02-01 19:56:58 -0300

    Creating a var with one subject

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require("express")
 
 const app = express()
 
+const resumos = [
+    {
+        id: 1,
+        materia: "Portugues",
+        texto: "Uma linguagem"
+    }
+]
+
 app.get("/login", (req, res) => {
     res.send("Receba")
 })
