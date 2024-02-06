b80a7b6e6230670c62f7f0ac5ab6876731ae8432
Luca Battesini <lucabattesini1@gmail.com>
2024-02-06 19:26:57 -0300

    Creating a new file to db

---
+++ db.js
@@ -0,0 +1,28 @@
+const resumos = [
+    {
+        id: 1,
+        materia: "Portugues",
+        texto: "Foco na língua portuguesa, incluindo gramática, literatura, interpretação de textos e produção textual."
+    },
+    {
+        id: 2,
+        materia: "Matematica",
+        texto: "Estuda-se números, álgebra, geometria, trigonometria e cálculo, desenvolvendo habilidades analíticas e de resolução de problemas."
+    },
+    {
+        id: 3,
+        materia: "História",
+        texto: "Analisa-se o desenvolvimento da humanidade ao longo do tempo, incluindo eventos históricos, culturais, políticos e sociais."
+    },
+    {
+        id: 4,
+        materia: "Geografia",
+        texto: "Estuda-se a Terra e seus fenômenos, incluindo clima, relevo, recursos naturais, populações e questões geopolíticas."
+    },
+    {
+        id: 5,
+        materia: "Biologia",
+        texto: "Explora-se a vida, desde os níveis molecular e celular até a ecologia, abrangendo organismos, sistemas biológicos e processos evolutivos."
+    }]
+
+module.exports = resumos
